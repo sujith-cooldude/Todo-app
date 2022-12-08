1cf064f62bb2f31dee8c57e01e9534a544d7d547
Sujithkumar <sujithkumar.parameshwaran@gramener.org>
2022-12-08 13:43:54 +0530

    crud method created using DRF

--- apis/urls.py
+++ apis/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import taskList
+from .views import taskList, taskDetail, taskUpdate, taskCreate
 
 urlpatterns = [
-    path("task-list/",taskList,name="task-list"),
+    path("task-list/", taskList, name="task-list"),
+    path("task-detail/<str:pk>", taskDetail, name="task-detail"),
+    path("task-update/<str:pk>", taskUpdate, name="task-update"),
+    path("task-create/", taskCreate, name="task-create"),
 ]
--- apis/views.py
+++ apis/views.py
@@ -1,7 +1,31 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .serializers import TaskSerializer
+from base.models import Task
 
 @api_view(["GET"])
 def taskList(request):
-    task = {"name":"mock data meeting","age":26}
-    return Response(task)
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks,many=True)
+    return Response(serializer.data)
+
+@api_view(["GET"])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(tasks,many=False)
+    return Response(serializer.data)
+
+@api_view(["PUT"])
+def taskUpdate(request,pk):
+    tasks = Task.objects.get(pk=pk)
+    serializer = TaskSerializer(instance=tasks,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(["POST"])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
